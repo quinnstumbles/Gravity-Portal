026b627ebc7694a9a3fe56333f9fbfdbdcd9e740
Robert Nubla <rnubla97@gmail.com>
2020-12-05 00:44:08 -0500

    Almost working Portal

--- Assets/ModAssets/PortalMods/Scripts/PortalCamera.cs
+++ Assets/ModAssets/PortalMods/Scripts/PortalCamera.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PortalCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform playerCamera;
+    public Transform portalWorld2;
+    public Transform portalWorld1;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerOffsetFromPortal = playerCamera.position - portalWorld1.position;
+        transform.position = portalWorld2.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portalWorld2.rotation, portalWorld1.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 }
