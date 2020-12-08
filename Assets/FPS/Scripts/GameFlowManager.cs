0ba0ea4d3ff55bb6333dd11c60bfd31b922acea3
Quinn Huang <quinnstumbles@gmail.com>
2020-12-08 16:48:36 -0500

    allows scene switch when complete a level

--- Assets/FPS/Scripts/GameFlowManager.cs
+++ Assets/FPS/Scripts/GameFlowManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class GameFlowManager : MonoBehaviour
 {
+
     [Header("Parameters")]
     [Tooltip("Duration of the fade-to-black at the end of the game")]
     public float endSceneLoadDelay = 3f;
@@ -25,8 +26,11 @@ public class GameFlowManager : MonoBehaviour
     [Tooltip("This string has to be the name of the scene you want to load when losing")]
     public string loseSceneName = "LoseScene";
 
+    [Header("Set in Inspector")]
+    public int moreStages;
 
     public bool gameIsEnding { get; private set; }
+
 
     PlayerCharacterController m_Player;
     NotificationHUDManager m_NotificationHUDManager;
@@ -43,33 +47,32 @@ public class GameFlowManager : MonoBehaviour
 		DebugUtility.HandleErrorIfNullFindObject<ObjectiveManager, GameFlowManager>(m_ObjectiveManager, this);
 
         AudioUtility.SetMasterVolume(1);
+
     }
 
     void Update()
     {
-        if (gameIsEnding)
+        // Test if player died
+        if (m_Player.isDead)
+        {
+            EndGame(false);
+        }
+
+
+        if (m_ObjectiveManager.AreAllObjectivesCompleted() && moreStages != 0)
+        { SceneManager.LoadScene("SecondaryScene"); }
+
+        else if (m_ObjectiveManager.AreAllObjectivesCompleted() && moreStages == 0)
         {
             float timeRatio = 1 - (m_TimeLoadEndGameScene - Time.time) / endSceneLoadDelay;
             endGameFadeCanvasGroup.alpha = timeRatio;
 
             AudioUtility.SetMasterVolume(1 - timeRatio);
-
-            // See if it's time to load the end scene (after the delay)
-            if (Time.time >= m_TimeLoadEndGameScene)
-            {
-                SceneManager.LoadScene(m_SceneToLoad);
-                gameIsEnding = false;
-            }
+            EndGame(true);
         }
-        else
-        {
-            if (m_ObjectiveManager.AreAllObjectivesCompleted())
-                EndGame(true);
 
-            // Test if player died
-            if (m_Player.isDead)
-                EndGame(false);
-        }
+
+
     }
 
     void EndGame(bool win)
@@ -83,7 +86,6 @@ public class GameFlowManager : MonoBehaviour
         endGameFadeCanvasGroup.gameObject.SetActive(true);
         if (win)
         {
-            m_SceneToLoad = winSceneName;
             m_TimeLoadEndGameScene = Time.time + endSceneLoadDelay + delayBeforeFadeToBlack;
 
             // play a sound on win
@@ -92,6 +94,8 @@ public class GameFlowManager : MonoBehaviour
             audioSource.playOnAwake = false;
             audioSource.outputAudioMixerGroup = AudioUtility.GetAudioGroup(AudioUtility.AudioGroups.HUDVictory);
             audioSource.PlayScheduled(AudioSettings.dspTime + delayBeforeWinMessage);
+
+            SceneManager.LoadScene(winSceneName);
 
             // create a game message
             var message = Instantiate(WinGameMessagePrefab).GetComponent<DisplayMessage>();
@@ -103,7 +107,7 @@ public class GameFlowManager : MonoBehaviour
         }
         else
         {
-            m_SceneToLoad = loseSceneName;
+            SceneManager.LoadScene(loseSceneName);
             m_TimeLoadEndGameScene = Time.time + endSceneLoadDelay;
         }
     }
